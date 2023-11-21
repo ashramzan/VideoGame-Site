590ab214b5669549a59659a5c737591cef1408c1
ashramzan <ashalramzan@yahoo.co.uk>
2023-11-21 23:32:09 +0000

    added new pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './components/pages/home';
+import SignUp from './components/pages/signup';
+import Consoles from './components/pages/consoles';
+import Games from './components/pages/games';
+
 
 function App() {
   return (
@@ -11,6 +15,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' exact Component={Home}/>
+          <Route path='consoles' Component={Consoles}/>
+          <Route path='games' Component={Games}/>
+          <Route path='sign-up' Component={SignUp}/>
         </Routes>    
         </Router>   
     </>
---
+++ src/components/pages/consoles.js
@@ -0,0 +1,6 @@
+import React from "react";
+import '../../App.css';
+
+export default function Consoles(){
+    return <h1 className="consoles">CONSOLES</h1>;
+}
